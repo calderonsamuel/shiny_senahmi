ce5209df6834dc40f0c3b13af558301617257d28
calderonsamuel <samuel.calderon@uarm.pe>
2019-09-09 22:43:50 +0000

    se borró código innecesario y comentado. usar un punto dentro de map generaba error en la creación de la data

--- app.R
+++ app.R
@@ -1,11 +1,8 @@
+library(tidyverse)
 library(shiny)
-library(readr)
-library(dplyr)
-library(ggplot2)
 library(shinythemes)
 library(DT)
 library(writexl)
-library(tidyr)
 library(readxl)
 
 # setwd("C:\\Users\\Win7\\Documents\\samuel\\R\\Pruebas\\Shiny\\ver1-0")
@@ -13,7 +10,7 @@ library(readxl)
 estaciones <- read_xlsx("bundle/estaciones.xlsx")%>%
     mutate(files = paste0("bundle/", region, "_", estacion, ".csv"))
 
-files <- estaciones$files
+# files <- estaciones$files
 
 ui <- fluidPage(title = "Shiny app",
                 
@@ -170,20 +167,12 @@ server <- function(input, output) {
         {
             req(input$z)
             
-            indice <- estaciones %>%
-                dplyr::filter(region %in% input$z)
-            
-            data <- read_csv(file = indice$files[1], col_types = "Dcccd")
-            if(length(indice$files) == 1){
-                data <- data
-            } else {
-            for (i in 2:length(indice$files)) {
-                data <- bind_rows(data, read_csv(file = indice$files[i], col_types = "Dcccd"))
-                                            }
-                }
-            data %>%
-                dplyr::filter(variable %in% input$y, fecha >= input$date[1] & fecha <= input$date[2])
-            
+            data <- estaciones %>%
+                dplyr::filter(region %in% input$z) %>%
+                .$files %>%
+                map_df(read_csv, col_types = "Dcccd") %>%
+                dplyr::filter(variable %in% input$y, 
+                              fecha >= input$date[1] & fecha <= input$date[2])
         })
     
     plot_title <- eventReactive(
@@ -202,7 +191,6 @@ server <- function(input, output) {
                  title = plot_title(),
                  color = "Región",
                  caption = "Elaboración propia con base en datos del Senahmi") +
-            # ggtitle(plot_title()) +
             theme(plot.title = element_text(face = 'bold', hjust = 0.5),
                   plot.caption = element_text(hjust = 0))
         
@@ -233,7 +221,9 @@ server <- function(input, output) {
                       col_names = TRUE, 
                       na = "")
         } else if(filetype == "xlsx"){
-            write_xlsx(x = data, path = file, format_headers = FALSE)
+            write_xlsx(x = data, 
+                       path = file, 
+                       format_headers = FALSE)
         }
     }
     
